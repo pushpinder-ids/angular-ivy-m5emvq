9c1f8c1a4cdcbfa05d97bbd3e1dcfa9d0a4e99de
pushpinder-ids <pushpinder.ids@gmail.com>
2022-01-16 15:00:55 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+ngOnInit(): void {
+of(1,2,3,4)
+.pipe(
+  map(x=>x*2),
+  tap(console.log),
+  take(2)
+  )
+  .subscribe()
+}
   name = 'Angular ' + VERSION.major;
 }
